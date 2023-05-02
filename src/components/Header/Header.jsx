974a9165f8c9aab17fb1710b596c7965a78514c4
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-05-02 22:29:02 +0600

    navber complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,21 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <nav>
-            
+            <div className="top-0 p-5 justify-between h-16 flex text-white bg-black">
+                <div>
+                    <a className=" normal-case text-xl">COOKBOOKS</a>
+                </div>
+                <div className="flex gap-4  ">
+
+                    <Link to='/'>Home</Link>
+                    <Link to='/blog'>Blog</Link>
+                    <Link to='/login'>Login</Link>
+                    <Link to='/register'>Register</Link>
+                </div>
+            </div>
+            <div className=''>
+                    <img className='w-full ' src="https://img.freepik.com/free-photo/pasta-colored-farfalle-salad-with-tomatoes-mozzarella-basil_2829-14368.jpg?w=900&t=st=1683043134~exp=1683043734~hmac=7df8d0b1425453a069fe1f46d67450b93fb590fffffba414587c3541dc491504" alt="" />
+                </div>
         </nav>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,29 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './components/Home/Home.jsx';
+import Blog from './routes/Blog/Blog.jsx';
+import Login from './routes/Login/Login.jsx';
+import Register from './routes/Register/Register.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home></Home>,
+    children: [
+      {
+        path: "blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 

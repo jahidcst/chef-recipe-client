f03eb1b1c3a1c368cb5bb314a22782f8f7f301bf
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-05-04 18:07:02 +0600

    firebase install

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWevZQ5_VmGEIh4jB2Yt-JMRmlz6J_lUY",
+  authDomain: "chef-recipe-project-da04f.firebaseapp.com",
+  projectId: "chef-recipe-project-da04f",
+  storageBucket: "chef-recipe-project-da04f.appspot.com",
+  messagingSenderId: "319778817245",
+  appId: "1:319778817245:web:04b019b38287822be88734"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
